a8611140b4b5584a8ea48f286df4d8d08ed6f2b8
yogi joshi <y2joshi@uwaterloo.ca>
2015-06-29 02:52:29 -0400

    example files uploaded for demo

--- example.sh
+++ example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CLASSPATH=.; export CLASSPATH=$CLASSPATH$(find . -name '*.jar' -type f -printf ':%p\n' | sort -u | tr -d '\n'); echo $CLASSPATHPATH
-java rithm.driver.RiTHMBrewer -configFile /home/y2joshi/rithm_1.properties
+java rithm.driver.RiTHMBrewer -configFile rithm_1.properties
